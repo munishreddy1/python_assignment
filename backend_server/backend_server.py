5ff267d364b66826715bb665b42c79bae06be77c
munishreddy1 <munish.modugula@gmail.com>
2023-07-01 15:59:05 +0200

    Updated Env Variables

--- backend_server/backend_server.py
+++ backend_server/backend_server.py
@@ -1,22 +1,15 @@
 from flask import Flask, jsonify
 import psycopg2
-
+import os
 app = Flask(__name__)
-
-# Connection parameters for PostgreSQL
-hostname = 'host.docker.internal'
-port = 5432
-database = 'postgres'
-username = 'postgres'
-password = 'password'
 
 # Connect to the PostgreSQL database
 connection = psycopg2.connect(
-    host=hostname,
-    port=port,
-    database=database,
-    user=username,
-    password=password
+    host=os.environ.get('DB_HOST'),
+    port=os.environ.get('DB_PORT'),
+    database=os.environ.get('DB_NAME'),
+    user=os.environ.get('DB_USERNAME'),
+    password=os.environ.get('DB_PASSWORD')
 )
 
 # Create a cursor object
@@ -39,20 +32,20 @@ def fetch_sensor_data():
     average_temperature = total_temperature / len(rows)
     average_humidity = total_humidity / len(rows)
 
-    # # Find highest and lowest values
-    # highest_temperature = max(rows, key=lambda x: x[0])[0]
-    # lowest_temperature = min(rows, key=lambda x: x[0])[0]
-    # highest_humidity = max(rows, key=lambda x: x[1])[1]
-    # lowest_humidity = min(rows, key=lambda x: x[1])[1]
+    # Find highest and lowest values
+    highest_temperature = max(rows, key=lambda x: x[0])[0]
+    lowest_temperature = min(rows, key=lambda x: x[0])[0]
+    highest_humidity = max(rows, key=lambda x: x[1])[1]
+    lowest_humidity = min(rows, key=lambda x: x[1])[1]
 
     # Prepare the response
     processed_data = {
         'average_temperature': average_temperature,
         'average_humidity': average_humidity,
-        # 'highest_temperature': highest_temperature,
-        # 'lowest_temperature': lowest_temperature,
-        # 'highest_humidity': highest_humidity,
-        # 'lowest_humidity': lowest_humidity
+        'highest_temperature': highest_temperature,
+        'lowest_temperature': lowest_temperature,
+        'highest_humidity': highest_humidity,
+        'lowest_humidity': lowest_humidity
     }
 
     return processed_data
@@ -67,3 +60,11 @@ def get_processed_data():
 
 if __name__ == '__main__':
     app.run()
+
+
+
+# hostname = 'host.docker.internal'
+# port = 5432
+# database = 'postgres'
+# username = 'postgres'
+# password = 'password'
